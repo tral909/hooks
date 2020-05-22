82b133256ac3e89e2c85934eb8187ea36b54a31a
tral909 <indorm1992@mail.ru>
2020-05-22 14:53:54 +0300

    Consider useState hook

--- src/index.js
+++ src/index.js
@@ -1,6 +1,74 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => { return('HI!') };
+const App = () => {
+  return (
+    <div>
+      <HookSwitcher />
+    </div>
+  );
+};
+
+const HookSwitcher = () => {
+  const [ color, setColor ] = useState('grey');
+  const [ fontSize, setFontSize ] = useState(14);
+
+  return (
+    <div style={{ padding: '10px',
+      backgroundColor: color,
+      fontSize: `${fontSize}px`
+     }}>
+      Hello world
+      <button
+        onClick={() => setColor('grey')}>
+        Dark
+      </button>
+      <button
+        onClick={() => setColor('white')}>
+        Light
+      </button>
+      <button
+        onClick={() => setFontSize((s) => s - 2)}>
+        Smaller
+      </button>
+      <button
+        onClick={() => setFontSize((s) => s + 2)}>
+        Larger
+      </button>
+    </div>
+  );
+};
+
+class PersonClass extends Component {
+  state = {
+    firstName: 'Bob',
+    lastName: 'Smith'
+  }
+
+  updateFirstName() {
+    this.setState({ firstName: 'Mike'});
+  }
+
+}
+
+const Person = () => {
+  const [ person, setPerson ] = useState({
+    firstName: 'Bob',
+    lastName: 'Smith'
+  });
+
+  // в отличие от setState - lastName затрется!!
+  setPerson({ firstName: 'Mike' });
+  // Так заработает
+  setPerson((person) => {
+    return { ...person, firstName: 'Mike'};
+  });
+
+  // или разделить проперти по useState
+  // const [ firstName, setFirstName ] = useState('Bob');
+  // const [ lastName, setLastName ] = useState('Bob');
+
+  // setFirstName('Mike');
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
